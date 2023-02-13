cd2a2a7b3b8ee5ff3197d93c347eb3d29cf38011
pgrrr <pgrrr119@gmail.com>
2023-02-13 21:40:16 +0900

    [#1]feat: 유저 카카오톡 로그인 API

--- src/main/java/com/pgrrr/woohaha/user/service/UserService.java
+++ src/main/java/com/pgrrr/woohaha/user/service/UserService.java
@@ -1,5 +1,8 @@
 package com.pgrrr.woohaha.user.service;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.pgrrr.woohaha.mapper.UserMapper;
 import com.pgrrr.woohaha.user.domain.User;
 import com.pgrrr.woohaha.user.dto.UserJoinRequestDto;
@@ -10,6 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.Optional;
 
 @Service
@@ -47,5 +54,98 @@ public class UserService {
         updateRequest.setPwd(EncryptSha256.encrypt(updateRequest.getPwd()));
         userMapper.update(updateRequest.toEntity());
         return new UserResponseDto(user);
+    }
+
+    public String getAccessToken(String authorize_code) {
+        String access_token = "";
+        String refresh_token = "";
+        String reqURL = "https://kauth.kakao.com/oauth/token";
+
+        try {
+            URL url = new URL(reqURL);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            StringBuilder sb = new StringBuilder();
+            sb.append("grant_type=authorization_code");
+            sb.append("&client_id=7ab07adcdacec7c4752add19a162ee1b");
+            sb.append("&redirect_uri=http://localhost:8080/members/kakao");
+            sb.append("&code=" + authorize_code);
+            bw.write(sb.toString());
+            bw.flush();
+
+            int responseCode = conn.getResponseCode();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            access_token = element.getAsJsonObject().get("access_token").getAsString();
+            refresh_token = element.getAsJsonObject().get("refresh_token").getAsString();
+
+
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return access_token;
+    }
+
+    public HashMap<String, Object> getUserInfo(String access_Token) {
+
+        HashMap<String, Object> userInfo = new HashMap<String, Object>();
+        String reqURL = "https://kapi.kakao.com/v2/user/me";
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+
+            int responseCode = conn.getResponseCode();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+            JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+
+            long id = element.getAsJsonObject().get("id").getAsLong();
+            String nickname = properties.getAsJsonObject().get("nickname").getAsString();
+            String email = kakao_account.getAsJsonObject().get("email").getAsString();
+//            String age = kakao_account.getAsJsonObject().get("age_range").getAsString();
+            String gender = kakao_account.getAsJsonObject().get("gender").getAsString();
+
+            userInfo.put("nickname", nickname);
+            userInfo.put("email", email);
+//            userInfo.put("age", age);
+            userInfo.put("id", id);
+            userInfo.put("gender", gender);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return userInfo;
     }
 }

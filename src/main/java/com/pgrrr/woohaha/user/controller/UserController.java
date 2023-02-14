459000cb2721cbf166a3b5a94f00a2c62e3326ca
pgrrr <pgrrr119@gmail.com>
2023-02-14 21:23:25 +0900

    [#2]feat: 유저 카카오톡 로그인 API

--- src/main/java/com/pgrrr/woohaha/user/controller/UserController.java
+++ src/main/java/com/pgrrr/woohaha/user/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.pgrrr.woohaha.user.controller;
 
 import com.pgrrr.woohaha.user.dto.UserJoinRequestDto;
+import com.pgrrr.woohaha.user.dto.UserResponseDto;
+import com.pgrrr.woohaha.user.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/users")
@@ -35,4 +41,26 @@ public class UserController {
     public String join(UserJoinRequestDto requestDto) {
         return "redirect:/";
     }
+
+    @GetMapping("/kakao")
+    public String sns(@RequestParam(value = "code", required = false) String code, UserResponseDto responseDto, HttpSession session, Model model) {
+//        String access_Token = UserService.getAccessToken(code);
+//        HashMap<String, Object> userInfo = UserService.getUserInfo(access_Token);
+//        String email = (String) userInfo.get("email");
+        try {
+//            UserResponseDto memberResponse = UserService(email);
+//            session.setAttribute("memberResponse", memberResponse);
+            return "redirect:/";
+        } catch (NullPointerException e) {
+//            String name = (String) userInfo.get("nickname");
+//            String gender = (String) userInfo.get("gender");
+//            responseDto.getEmail(email);
+//            responseDto.getName(name);
+//            responseDto.getId(gender);
+            model.addAttribute("kakao", responseDto);
+            return "/member/signUp";
+        }
+    }
 }
+
+
